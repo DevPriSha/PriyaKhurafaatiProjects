2b74558ddf5be48af8c3fcbac613e23531150a60
Priya Sharma <77352100+DevPriSha@users.noreply.github.com>
2021-04-27 10:15:29 +0530

    Updated whatsappautomation to hide friend name

--- WhatsappAutomation.py
+++ WhatsappAutomation.py
@@ -7,13 +7,14 @@ import time
 driver=webdriver.Chrome("D:\Downloads\chromedriver.exe")
 driver.get("https://web.whatsapp.com/")
 wait=WebDriverWait(driver,600)
-target='"CSAI 20 Shweta"'
-string = 'HI! I am Priya. I love Olivia <3'
+target='"Aaj 5 bje imp meet h"'
+string=' This group has been hacked. It will now be deleted after  '
+string2=" messages Σ3"
 x_arg='//span[contains(@title, '+target+')]'
 target=wait.until(EC.presence_of_element_located((By.XPATH, x_arg)))
 target.click()
 
 input_box=driver.find_element_by_class_name("_2A8P4")
 
-for i in range(5):
-    input_box.send_keys(string+Keys.ENTER)
+for i in range(200,1,-1):
+    input_box.send_keys(string+str(i)+string2+Keys.ENTER)
