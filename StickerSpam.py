24d3177657b0539635ba3aa41aa36b68f609b632
Priya Sharma <77352100+DevPriSha@users.noreply.github.com>
2021-05-01 23:42:26 +0530

    added comments and made code more user-friendly

--- StickerSpam.py
+++ StickerSpam.py
@@ -1,3 +1,4 @@
+#import stuff
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -5,23 +6,42 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import time
 
-driver=webdriver.Chrome("D:\Downloads\chromedriver.exe")
-driver.get("https://web.whatsapp.com/")
+# set variables
+target='"Friend"' #contact or group name where you want to send messages (make sure to not mess with the quotes)
+times_scroll = 6 #refer delimiter comment below
+times_repeat = 100 #refer delimiter comment below
+chromeDriverLocation = "D:\Downloads\chromedriver.exe" #change it with location where you saved chrome driver (most likely downloads folder)
+
+#actual code
+driver=webdriver.Chrome(chromeDriverLocation) #opens chrome
+driver.get("https://web.whatsapp.com/") #opens whatsapp web, you'll have to manually scan qr code to login
 wait=WebDriverWait(driver,600)
-target='"+91 80103 56367"'
 x_arg='//span[contains(@title, '+target+')]'
-target=wait.until(EC.presence_of_element_located((By.XPATH, x_arg)))
-target.click()
+target=wait.until(EC.presence_of_element_located((By.XPATH, x_arg))) #stores location of chat of target
+target.click() #open target chat (Note: if chat doesn't open, try scrolling on the automated browser to bring target contact to view)
 
 emojimenu=wait.until(EC.presence_of_element_located((By.XPATH,'//*[@id="main"]/footer/div[1]/div[1]/div[1]')))
-emojimenu.click()
+emojimenu.click() #opens emoji menu
 stickermenu=wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="main"]/footer/div[1]/div[1]/div[1]/button[4]')))
-stickermenu.click()
+stickermenu.click() #opens sticker menu
 x_arg_begin = '//*[@id="main"]/footer/div[2]/div/div[3]/div[1]/div/div[1]/div[2]/div/div['
 x_arg_end=']/div/span/img'
 
-for i in range(6): #times scroll
-    for a in range(2): #times repeat
+"""
+So how do the stickers get selected?
+basically, the first menu to pop up in the stickers menu is the recent stickers used one. 
+We'll be spamming few recent ones. 
+The first (and the most recent) sticker has div[1], the second has div[2] and so on. 
+
+Now we can technically run a loop till *x* recent stickers but stickers that are not visible in the browser won't send unless you manually scroll.
+And manually scrolling everytime can be a pain. 
+So, I have initialised 3 nested loops that send 4 stickers at a time, for the number of times you  want to send each sticker aka times_repeat and a total of times_scroll*4 stickers sent times_repeat times.
+
+If you want to collate, shift the second loop with the third loop (collate means if you want each sticker to be sent times_repeat times before the new sticker is sent.)
+"""
+
+for i in range(times_scroll): #times scroll
+    for a in range(times_repeat): #times repeat
         for j in range(1,5): #times send
-            sticker=wait.until(EC.presence_of_element_located((By.XPATH, x_arg_begin+str(6*i+j)+x_arg_end)))
+            sticker=wait.until(EC.presence_of_element_located((By.XPATH, x_arg_begin+str((times_scroll*i)+j)+x_arg_end)))
             sticker.click()
--- WhatsappAutomation.py
+++ WhatsappAutomation.py
@@ -1,20 +1,28 @@
+#import modules
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import time
-driver=webdriver.Chrome("D:\Downloads\chromedriver.exe")
-driver.get("https://web.whatsapp.com/")
+
+#set variables
+target='"Friend"' #contact or group name where you want to send messages (make sure to not mess with the quotes)
+chromeDriverLocation = "D:\Downloads\chromedriver.exe" #change it with location where you saved chrome driver (most likely downloads folder)
+stringStart=' This group has been hacked. It will now be deleted after  ' #beginning of message
+stringEnd=" messages Σ3" #ending of message
+textBoxClassName = "_2A8P4" #different for different users, use inspect elements to find yours
+timesSpam = 1000 #number of times a message should be spammed
+
+#actual code
+driver=webdriver.Chrome(chromeDriverLocation) #opens chrome
+driver.get("https://web.whatsapp.com/") #opens whatsapp web, you'll have to manually scan qr code to login
 wait=WebDriverWait(driver,600)
-target='"Aaj 5 bje imp meet h"'
-string=' This group has been hacked. It will now be deleted after  '
-string2=" messages Σ3"
-x_arg='//span[contains(@title, '+target+')]'
-target=wait.until(EC.presence_of_element_located((By.XPATH, x_arg)))
-target.click()
+x_arg='//span[contains(@title, '+target+')]' 
+target=wait.until(EC.presence_of_element_located((By.XPATH, x_arg))) #stores location of chat of target
+target.click() #open target chat (Note: if chat doesn't open, try scrolling on the automated browser to bring target contact to view)
 
-input_box=driver.find_element_by_class_name("_2A8P4")
+input_box=driver.find_element_by_class_name(textBoxClassName) 
 
-for i in range(200,1,-1):
-    input_box.send_keys(string+str(i)+string2+Keys.ENTER)
+for i in range(timesSpam):
+    input_box.send_keys(string+str(timesSpam-i)+string2+Keys.ENTER) #you can set this acc to your tastes. DO NOT REMOVE +Keys.ENTER
